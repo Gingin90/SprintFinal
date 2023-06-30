8478b6947c0c0d25c807c0c068bb0f9aeccee723
ginger cea <gingercea@gmail.com>
2023-06-29 23:53:31 -0400

    Sprint Final M4 falta mejorar el diseño y darle funcionalidad en la actividad dos mail y button

--- app/src/main/java/com/example/sprintfinal/MainActivity.java
+++ app/src/main/java/com/example/sprintfinal/MainActivity.java
@@ -3,22 +3,32 @@ package com.example.sprintfinal;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.sprintfinal.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding =ActivityMainBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-        setContentView(view);
-
-       initListener();
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initListener();
 
     }
 
     private void initListener() {
+    binding.buttonNext.setOnClickListener(v -> {
+
+        Intent goTo = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(goTo);
+    });
+
+}
+
+}
--- app/src/main/java/com/example/sprintfinal/SecondActivity.java
+++ app/src/main/java/com/example/sprintfinal/SecondActivity.java
@@ -2,24 +2,68 @@ package com.example.sprintfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import com.example.sprintfinal.databinding.ActivityMainBinding;
+
 import com.example.sprintfinal.databinding.ActivitySecondBinding;
 
-public class SecondActivity extends AppCompatActivity {
 
+public class SecondActivity extends AppCompatActivity {
+    private ActivitySecondBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivitySecondBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        ActivitySecondBinding binding = ActivitySecondBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-        setContentView(view);
+
+        initListener();
+
     }
 
-}
+    private void initListener() {
 
+        binding.buttonLinkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+                String linkedInUrl = "https://www.linkedin.com/in/ginger-cea-zamora/";
+                Uri uri = Uri.parse(linkedInUrl);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        binding.buttonWhatsApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View buttonLinkedin) {
+                String numeroTelefono = "+56989147834";
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + numeroTelefono));
+                startActivity(intent);
+
+                }
+        });
+
+       binding.buttonMail.setOnClickListener(new View.OnClickListener(){
+
+           @Override
+           public void onClick(View v) {
+
+           }
+       });
+// Creación de método con intent implícito para enviar correo electrónico.
+        public void composeEmail(String, String subject) {
+            Intent implicitIntentEmail = new Intent(Intent.ACTION_SENDTO);
+            implicitIntentEmail.setData(Uri.parse("mailto:"));
+            implicitIntentEmail.putExtra(Intent.EXTRA_EMAIL, addresses);
+            implicitIntentEmail.putExtra(Intent.EXTRA_SUBJECT, subject);
+            if (implicitIntentEmail.resolveActivity(getPackageManager()) != null) {
+                startActivity(implicitIntentEmail);
+            }
+        });
+}
 
 
